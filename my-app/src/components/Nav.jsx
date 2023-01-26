e51a8e9dbc7a391b9e299b0d7039b9492e9e9074
C4m0mila <112404881+C4m0mila@users.noreply.github.com>
2023-01-26 20:45:37 -0300

    fixing path error

--- my-app/src/components/Nav.jsx
+++ my-app/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../src/nav.css'
+import '../css/nav.css'
 class Nav extends React.Component {
     constructor(props) {
         super(props)
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 // arquivos gerados automáticamente através do comando 'npx create-react-app my-app'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './css/index.css';
 
 import reportWebVitals from './reportWebVitals';
 // arquivos gerados automáticamente através do comando 'npx create-react-app my-app'
