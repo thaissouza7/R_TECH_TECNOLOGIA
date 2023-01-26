2da373df257ebd4937250d73c433608e1c0b5ca3
thaissouza7 <107569856+thaissouza7@users.noreply.github.com>
2023-01-25 22:53:45 -0300

    Template 1.0
    - implementação inicial de header, nav, footer

---
+++ RTECH_TECNOLOGIA/my-app/src/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../src/nav.css'
+class Nav extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+render(){
+    return <div>
+         <nav id="nav">
+        <ul>
+            <a href="cardapio.html">PRODUTOS</a>
+            <a href="cardapio.html">SOBRE NÓS</a>
+            <a href="contato.html">CONTATOS</a>
+        </ul>
+    </nav>
+       </div>
+    }
+}
+
+export default Nav
