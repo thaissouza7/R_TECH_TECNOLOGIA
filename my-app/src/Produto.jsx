a097067bbe54e921da30717b291e24cbac1a561e
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2023-01-26 11:14:19 -0300

    Add files via upload

---
+++ my-app/src/Produto.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './produto.css'
+
+class Produto extends React.Component{
+    constructor(props){
+        super(props)
+        this.titulo = this.props.titulo
+        this.description = this.props.description
+        this.imgProduct = this.props.imgProduct
+    }
+
+    render(){
+        return <div className="caixaProduct">
+            <img src={this.imgProduct} alt="" className="imgProduct"/>
+            <h4 className="tituloProduct">{this.titulo}</h4>
+            <p className="descriptionProduct">{this.description}</p>
+            <button className="btnComprar">Comprar</button>
+        </div>
+    }
+}
+
+export default Produto
